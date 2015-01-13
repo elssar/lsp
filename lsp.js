81d796faf53ddb5c1d7dd975f7f008cb09493695
elssar <elssar@altrawcode.com>
2015-01-13 09:58:39 +0530

    Added support for parsing arguments  - Muted hidden files and directories by default, can be unmuted by passing a command line option  - Can specify depth. Default is set to 0
    Files and directories are printed in different color text
    
    Bumped up version to 0.1.0

--- lsp.js
+++ lsp.js
@@ -1,30 +1,44 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    colors = require('colors'),
+    program = require('commander');
 
-function makeSenseOfStats(name, baseDir) {
-    var data = {}
-    var full_path = path.join(baseDir, name)
-    stats = fs.statSync(full_path)
+program
+    .version('0.1.0')
+    .option('-a, --all', 'Show hidden files')
+    .option('-d, --depth <n>', 'Depth <int>', parseInt, 0)
+    .parse(process.argv);
+
+function makeSenseOfStats(name, baseDir, depth) {
+    var data = {},
+        full_path = path.join(baseDir, name);
+
+    stats = fs.statSync(full_path);
     if (stats.isDirectory()) {
-        data['name'] = name + '/',
-        data['is_dir'] = true,
-        data['contents'] = walkDir(full_path)
+        data['name'] = name + '/';
+        data['is_dir'] = true;
+        if (depth > 0) {
+            data['contents'] = walkDir(full_path, depth -1);
+        }
+    } else {
+        data['name'] = name;
+        data['is_dir'] = false;
     }
-    else {
-        data['name'] = name,
-        data['is_dir'] = false
-    }
+
     return data;
 }
 
-function walkDir(baseDir) {
+function walkDir(baseDir, depth, showHidden) {
     var dirTree = [];
-    names = fs.readdirSync(baseDir)
-    names.forEach(function(name){
-        dirTree.push(makeSenseOfStats(name, baseDir));
+    names = fs.readdirSync(baseDir).filter(function(name) {
+        return (showHidden || (name && (name[0] != '.')));
     });
+    names.forEach(function(name){
+        dirTree.push(makeSenseOfStats(name, baseDir, depth));
+    });
+
     return dirTree;
 }
 
@@ -32,15 +46,19 @@ function prettyPrint(data, indent) {
     var a = new Array(indent),
         padding = a.join(" ");
     data.forEach(function(datum) {
-        console.log(padding + datum.name);
         if(datum.is_dir) {
-            prettyPrint(datum.contents, indent+2);
+            console.log(padding + datum.name.underline.green);
+            if ('contents' in datum) {
+                prettyPrint(datum.contents, indent+3);
+            }
+        } else {
+            console.log(padding + datum.name.blue);
         }
     });
 }
 
 function lsp(baseDir) {
-    data = walkDir(baseDir);
+    data = walkDir(baseDir, program.depth, program.all);
     prettyPrint(data, 0);
 }
 
